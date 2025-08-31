5919fb431086f16ee477fe1fd011f1c99ad57d4d
muhashi <105213357+muhashi@users.noreply.github.com>
2025-08-31 20:37:06 +1000

    Update vite base directory

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // https://vitejs.dev/guide/static-deploy.html#github-pages
+  base: '/name-a-womandle/', // https://vitejs.dev/guide/static-deploy.html#github-pages
   plugins: [react()],
 });
